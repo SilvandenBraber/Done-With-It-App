ab777aa97b8d5cfd1dc6de7a86c80767f24643b5
SilvandenBraber <info@silvandenbraber.nl>
2024-09-20 16:44:39 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import AppLoading from "./app/components/AppLoading";
+import AppNavigator from "./app/navigation/AppNavigator";
+import AuthContext from "./app/auth/context";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import authStorage from "./app/auth/storage";
+import navigationTheme from "./app/navigation/navigationTheme";
+import { OfflineNotice } from "./app/components/detailComponents";
+import { navigationRef } from "./app/navigation/rootNavigation";
+import logger from "./app/utility/logger";
+
+logger.start();
+
+export default function App() {
+  const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
+
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
+  };
+
+  if (!isReady)
+    return (
+      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
+    );
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <OfflineNotice />
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
